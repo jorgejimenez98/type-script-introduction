7a25f2f17eb7d6b1a0b71d7581a7279084225948
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-08-25 14:37:07 +0200

    add json as code interface

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { Move, PokeAPIResponse } from '../interfaces/pokeapi-response.interface'
 export class Pokemon {
   /* public id: number = 1 // Init Value
   public name: string
@@ -30,8 +31,8 @@ export class Pokemon {
     console.log(`${this.name} ${this.name}`)
   }
 
-  async getMoves() {
-    const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/4')
+  async getMoves(): Promise<Move[]> {
+    const { data } = await axios.get<PokeAPIResponse>('https://pokeapi.co/api/v2/pokemon/4')
     return data?.moves
   }
 }
