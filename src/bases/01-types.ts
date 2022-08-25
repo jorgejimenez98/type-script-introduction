61bf4b963f69979fc16f5a772cc1d8c455ab0a3a
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-08-25 13:28:22 +0200

    add types

---
+++ src/bases/01-types.ts
@@ -0,0 +1,3 @@
+export const name = 'Jorge'
+export const age: number = 24
+export const isValid: boolean = true
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './style.css'
 import typescriptLogo from './typescript.svg'
 import { setupCounter } from './counter'
+import { name, age } from './bases/01-types'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -11,6 +12,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
     <h1>Vite + TypeScript</h1>
+    <h1>${name} ${age}</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
