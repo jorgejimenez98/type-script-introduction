79cf805214dda9d39033b6c9e14a3152a77fdb2c
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-08-25 13:35:04 +0200

    add types 2

--- src/bases/01-types.ts
+++ src/bases/01-types.ts
@@ -1,3 +1,15 @@
-export const name = 'Jorge'
+export let name: string = 'Jorge'
 export const age: number = 24
 export const isValid: boolean = true
+
+name = 'New NANE'
+
+export const templateString = `
+    Esto es un string multilinea 
+    que puede tener " dobles 
+    ' Simples
+    inyectar valores \$ ${name}
+    expresiones ${1 + 1}
+    numeros ${age}
+    booleanos ${isValid}
+`
